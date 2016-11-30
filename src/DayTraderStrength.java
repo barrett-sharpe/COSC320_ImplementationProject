f69f4dcbeedf05cbd1982502c26573e5d39c099f
Zach Petch <Zach@Zachs-MacBook-Pro.local>
2016-11-29 17:20:27 -0800

    Adding my thusfar failed attempt at a solution. (but did fix Algorithm A).

--- src/DayTraderStrength.java
+++ src/DayTraderStrength.java
@@ -1,101 +1,245 @@
-/*
- * File: 	DayTraderStrength.java
- * Purpose: This file represents a class containing the algorithms (and helper methods) required
- * 			to satisfy the parameters of the Project Page (see below).
- * Date: 	November 2016
- * Course: 	COSC 320 - Analysis of Algorithms
- * Project Page: https://people.ok.ubc.ca/yongg/teaching/cosc320/projects/project-design-and-implementation-tasks.pdf
- * 
- * Authors: (Group 6)
- * 			Barrett 
- * 			Brooke
- * 			Mark
- * 			Mitch
- * 			Zack
- */
+import java.util.ArrayList;
+
 public class DayTraderStrength {
 	
-	//Constants, to control the range of createRecord() results    
-	public Integer RANGE = 120;
-	public Integer COST = 60;
-		
-	//Constructors
-	public DayTraderStrength(){	};
-	public DayTraderStrength(int range, int cost){ RANGE = range; COST = cost;	};
+//	public int algorithmAforStrength(int [] profits) {
+//		int sum = 0;
+//		int curmax = 0;
+//		
+//		for(int i=1;i<profits.length;i++){
+//			for(int n=0;n+i<profits.length;n++){
+//				sum = sumSubArray(n, n+i, profits);
+//				if (curmax < sum)
+//					curmax = sum;
+//			}
+//		}
+//		return curmax;
+//	}
+//	
+//	private int sumSubArray(int a,int b, int[] arr) {
+//		int sum = 0;	
+//		for(int i=a;i<b;i++) {
+//			sum = sum + arr[i];
+//		}
+//		return sum;
+//	}
 	
-	//Methods
-	/**
-	 * createRecord
-	 * This method was provided to us by Dr. Yong Gao, and serves to generate an array of random integer values representing the trader's daily trade results (either a PROFIT (>0) , or a LOSS (<0) ).
-	 * 
-	 * @param int numRecords
-	 * @return int[]
-	 */
-	public int[] createRecord(int numEntries){    
-		long seed = System.currentTimeMillis();  
-		java.util.Random rand = new java.util.Random(seed);    
-		int[] a = new int[numEntries];    
-		for(int i = 0; i < numEntries; i++){        
-			a[i] = rand.nextInt(RANGE) - COST;
-		}      
-		 return a;
-	}
-	
-	/**
-	 * algorithmAforStrength
-	 * This method is the straightforward cubic time algorithm to compute the performance of the trader over all possible time periods, and then take the maximum of these values as the strength.
-	 * Maximum Subarray Problem: https://en.wikipedia.org/wiki/Maximum_subarray_problem
-	 * 
-	 * @param int[] profits
-	 * @return int strength
-	 */
-	public int algorithmAforStrength(int[] profits){
-		//Vars
-		int sum = 0;
-		int curmax = 0;
+	public int algorithmAforStrength(int [] profits)
+	{
+		int max = 0;
 		
-		for(int i=1;i<profits.length;i++){
-			for(int n=0;n+i<profits.length;n++){
-				sum =sumSubArray(n,n+i,profits);
-				if (curmax < sum)
-					curmax = sum;
+		for (int i = 0; i < profits.length; i++)
+		{
+			for (int j = i; j < profits.length; j++)
+			{
+				int sum = 0;
+				for (int k = i; k <= j; k++)
+				{
+					sum += profits[k];
+				}
+				max = Math.max(max, sum);
 			}
 		}
-		return curmax;
+		
+		return max;
 	}
+
+//	public int algorithmBforStrength(int [] profits)
+//	{
+//		// Get simplified profits (sequential positive and negative days are added together).
+//		ArrayList<Integer> condensedProfits = simplify(profits);
+//		
+//		// First entry is always positive, so the potentialMax is set to that.
+//		int potentialMax = condensedProfits.get(0);
+//		
+//		int lastMax = 0;
+//		
+//		/***************
+//		 * MAIN LOOP
+//		 * For each entry of condensedProfits (alternating between positive and negative),
+//		 * if the next negative and positive combine to be an improvement on potentialMax (i.e. are > 0)
+//		 * AND the current potentialMax is greater than the absolute value of the next negative,
+//		 * then combining all three get you the highest possible potentialMax so far.
+//		 * If the next negative and positive combine to less than 0, then the consecutive chain is broken
+//		 * and the current potentialMax is stored away (lastMax) to be checked against the next potentialMax
+//		 * either when the next consecutive chain is broken, or we reach the end of the condensedProfits
+//		 * list. If potentialMax > lastMax, lastMax gets the value of potentialMax. Then, finally, we return
+//		 * the greater of lastMax or potentialMax.
+//		 */
+//		
+//		int i = 0;
+//		
+//		// Only need to go through the loop if there is a next negative and positive left to check.
+//		while (i < condensedProfits.size() - 2)
+//		{
+//			// Setting quick-to-reference integers for next two values (since we always start with a positive,
+//			// and then alternate, we can know which is which).
+//			int neg = condensedProfits.get(i+1);
+//			int pos = condensedProfits.get(i+2);
+//			
+//			// If the two together will improve potentialMax...
+//			if (neg + pos >= 0)
+//			{
+//				// and if max + neg > 0...
+//				if (potentialMax + neg > 0)
+//				{
+//					// then max + neg + pos is a greater consecutive sum than either max or pos alone.
+//					potentialMax += neg + pos;
+//				}
+//				// else max + neg <= 0. So pos alone is at least as high as current max,
+//				else
+//				{
+//					potentialMax = pos;
+//				}
+//			}
+//			// neg is more negative than pos is positive.
+//			else
+//			{
+//				// make lastMax the greater of itself or potentialMax
+//				lastMax = Math.max(lastMax, potentialMax);
+//				potentialMax = pos;
+//			}
+//			
+//			// iterate by two (so we are looking at the next neg/pos pair, if both exist)
+//			i = i + 2;
+//		}
+//		
+//		return Math.max(lastMax, potentialMax);
+//	}
 	
-	/**
-	 * algorithmBforStrength
-	 * This method is the 'more efficient algorithm' to compute the performance of the trader over all possible time periods, and then take the maximum of these values as the strength.
-	 * 
-	 * @param int[] profits
-	 * @return int strength
-	 */
-	public int algorithmBforStrength(int[] profits){
-		//Variables
-		int sum = 0;
-		int maximum = 0;
-		//For
-		for(int i=0;i<profits.length;i++){
-			sum = Math.max(0, (sum + profits[i]));
-			maximum = Math.max(maximum, sum);
+	public int algorithmBforStrength(int [] profits)
+	{
+		// Get simplified profits (sequential positive and negative days are added together).
+		ArrayList<Integer> condensedProfits = simplify(profits);
+		
+		// First entry is always positive, so the potentialMax is set to that.
+		int potentialMax = condensedProfits.get(0);
+		
+		int lastMax = 0;
+		
+//		int otherPossibleMax = 0;
+		
+		/***************
+		 * MAIN LOOP
+		 * For each entry of condensedProfits (alternating between positive and negative),
+		 * if the next negative and positive combine to be an improvement on potentialMax (i.e. are > 0)
+		 * AND the current potentialMax is greater than the absolute value of the next negative,
+		 * then combining all three get you the highest possible potentialMax so far.
+		 * If the next negative and positive combine to less than 0, then the consecutive chain is broken
+		 * and the current potentialMax is stored away (lastMax) to be checked against the next potentialMax
+		 * either when the next consecutive chain is broken, or we reach the end of the condensedProfits
+		 * list. If potentialMax > lastMax, lastMax gets the value of potentialMax. Then, finally, we return
+		 * the greater of lastMax or potentialMax.
+		 */
+		
+		int i = 0;
+		
+		// Only need to go through the loop if there is a next negative and positive left to check.
+		while (i < condensedProfits.size() - 2)
+		{
+			// Setting quick-to-reference integers for next two values (since we always start with a positive,
+			// and then alternate, we can know which is which).
+			int neg = condensedProfits.get(i+1);
+			int pos = condensedProfits.get(i+2);
+			
+			if (neg + pos >= 0 || potentialMax + neg >= 0)
+			{
+				if (neg + pos >= 0 && potentialMax + neg > 0)
+				{
+					potentialMax += neg + pos;
+				}
+				else if (potentialMax + neg > 0)
+				{
+					if (potentialMax + neg + pos > pos)
+					{
+						potentialMax += neg + pos;
+					}
+				}
+				else
+				{
+					potentialMax = pos;
+				}
+			}
+			else
+			{
+				// make lastMax the greater of itself or potentialMax
+				lastMax = Math.max(lastMax, potentialMax);
+				potentialMax = pos;
+			}
+			
+			// iterate by two (so we are looking at the next neg/pos pair, if both exist)
+			i = i + 2;
 		}
-		return maximum;
+		
+		return Math.max(lastMax, potentialMax);
 	}
-	
-	//Method to sum subarray of an array
-	private int sumSubArray(int a,int b, int[] arr) {
-		int sum = 0;	
-		for(int i=a;i<b;i++) {
-			sum = sum + arr[i];
+
+	public ArrayList<Integer> simplify(int [] profits)
+	{
+		
+		ArrayList<Integer> simplifiedProfits = new ArrayList<Integer>();
+		
+		int i = 0;
+		
+		// ignore any starting days with non-positive profits
+		while (profits[i] <= 0 && i < profits.length)
+		{
+			i++;
 		}
-		return sum;
+		
+		// If they were not all negative profit days...
+		if (i < profits.length)
+		{
+			// Start the first entry to simplifiedProfits with the first profitable day.
+			int curVal = profits[i];
+			
+			boolean lastWasPositive = true;
+			
+			// Move to the next entry of profits
+			i++;
+			
+			/***************
+			 * MAIN LOOP
+			 * Here, for each iteration of the loop if the next element of profits
+			 * is the same sign (pos/neg) as the last, then its value is added
+			 * to the current entry of simplifiedProfits, if not, it starts
+			 * the next entry of simplifiedProfits.
+			 */
+			while (i < profits.length)
+			{
+				// Noting if this next element is non-negative.
+				boolean nextIsPositive = profits[i] >= 0;
+				
+				// If the next element has the same sign as the last element...
+				if (nextIsPositive == lastWasPositive)
+				{
+					// its value is added to the current entry of the simplifiedProfits list.
+					curVal += profits[i];
+				}
+				// Else, it's not the same element...
+				else
+				{
+					// So, the current simplified entry is added to the list,
+					simplifiedProfits.add(curVal);
+					
+					// the next element starts the next current entry,
+					curVal = profits[i];
+					
+					// and the (pos/neg) boolean is switched.
+					lastWasPositive = !lastWasPositive;
+				}
+				
+				i++;
+			}
+			// Add the final entry to simplifiedProfits
+			simplifiedProfits.add(curVal);
+		}
+		// Else, the trader never made a profit. simplifiedProfits list = { 0 }.
+		else
+		{
+			simplifiedProfits.add(0);
+		}
+
+		return simplifiedProfits;
 	}
-	
-	//Auto-generated getters and setters
-	public Integer getRANGE() {return RANGE;}
-	public void setRANGE(Integer rANGE) {RANGE = rANGE;}
-	public Integer getCOST() {return COST;}
-	public void setCOST(Integer cOST) {COST = cOST;}
-	
-}//class
+}
--- src/RunDayTraderStrength.java
+++ src/RunDayTraderStrength.java
@@ -1,129 +1,47 @@
-/*
- * File: 	RunDayTraderStrength.java
- * Purpose: This file is responsible for running an instance of DayTraderStrength.java class. 
- * 
- * Date: 	November 2016
- * Course: 	COSC 320 - Analysis of Algorithms
- * Project Page: https://people.ok.ubc.ca/yongg/teaching/cosc320/projects/project-design-and-implementation-tasks.pdf
- * 
- * Authors: (Group 6)
- * 			Barrett 
- * 			Brooke
- * 			Mark
- * 			Mitch
- * 			Zack
- */
+import java.util.Random;
+
 public class RunDayTraderStrength {
-	
-	//SpeedTest Counter
-	static int speedTestCounter = 0;
-	
-	//Helper Method: Array Printing
-	public static void printArray(int[] n){
-		System.out.print("[");
-		for(int i=0;i<n.length;i++){
-			//Space control, (maximum 4 = 3 + 1 for sign)
-			if(n[i]>=0){System.out.print(" ");} //sign space for +
-			if(Math.abs(n[i])< 10){
-				System.out.print("  "); //1-digit
-			}else if(Math.abs(n[i])< 100){
-				System.out.print(" ");//2-digit
-			}
-			System.out.print(n[i]+",");
-		}
-		System.out.println("]");
-	}
 
-	//Helper Method: Array Printing Override
-	public static void printArray(int[] n, int r, int c){
-		System.out.println("Range: "+r+"; Cost: "+c+";");
-		printArray(n);
-	}
-	
-	/**
-	 * speedTest
-	 * This method executes the algorithms, based on the input parameters, and displays the times
-	 * 
-	 * @param dts - The instance of DayTraderStrength, to run the algorithms from.
-	 * @param numRecords - The number of records for the algorithms to run.
-	 * @param numEntries - In each record to calculate, this is the number of entries per record.
-	 * 						Ex. 365 Entries per Record, means that a single record would represent a year's
-	 * 							worth of day net results. Stated another way, numEntries and numRecords
-	 * 							make up the lengths of a 2D array.
-	 * @param runAlgorithmA - Yes or No for running the (very slow) Algorithm A for a given speed test.
-	 */
-	public static void speedTest(DayTraderStrength dts, int numRecords, int numEntries, boolean runAlgorithmA){
-		//Print
-		speedTestCounter++;
-		if(runAlgorithmA){
-			System.out.println("[Speed Test "+speedTestCounter+"]: Algorithm A and B, running "+numRecords+" Records @ "+numEntries+" Entries/Record: ");
-		}else{
-			System.out.println("[Speed Test "+speedTestCounter+"]: ONLY Algorithm B, running "+numRecords+" Records @ "+numEntries+" Entries/Record: ");
-		}
-		//Arrays for strength results from A and B
-		int[] strengthsA = new int[numRecords];
-		int[] strengthsB = new int[numRecords];
-		
-		//-----------------------------------------------
-		//Algorithm A
-		//-----------------------------------------------
-		
-		if(runAlgorithmA){
-			//A) Start Time
-			long startTimeA = System.currentTimeMillis();
-					
-			//Run Algorithm A
-			for(int i=0; i < numRecords; i++){
-				//create records, compute and save Algo A results
-				int[] tmp = dts.createRecord(numEntries);
-				strengthsA[i]=dts.algorithmAforStrength(tmp);
-			}
-			
-			//A) End time, and print elapsed time
-			long stopTimeA = System.currentTimeMillis();
-			long elapsedTimeA = stopTimeA - startTimeA;
-			System.out.println("\tAlgorithm A: O(n^3) Poor Time Complexity\n\t\tTime for "+numRecords+" Records @ "+numEntries+"/Record: "+(elapsedTimeA/1000.0)+" seconds.\n");
-		}
-		
-		//-----------------------------------------------
-		//Algorithm B
-		//----------------------------------------------
-		
-		//B) Start Time
-		long startTimeB = System.currentTimeMillis();
-				
-		//Run Algorithm B
-		for(int i=0; i < numRecords; i++){
-			//create records, compute and save Algo B results
-			int[] tmp = dts.createRecord(numEntries);
-			strengthsB[i]=dts.algorithmBforStrength(tmp);
-		}
-				
-		//B) End time, and print elapsed time
-		long stopTimeB = System.currentTimeMillis();
-		long elapsedTimeB = stopTimeB - startTimeB;
-		System.out.println("\tAlgorithm B: Maximum Subarray (Kadane, 1984)\n\t\tTime for "+numRecords+" Records @ "+numEntries+"/Record: "+(elapsedTimeB/1000.0)+" seconds.\n");
-		
-	}
-	
-	//Main Method
 	public static void main(String[] args) {
-		//Print
+		// Print intro
 		System.out.println("File:\t\tRunDayTraderStrength.java\nPurpose:\tThis file is responsible for running an instance of DayTraderStrength.java class.\nDate:\t\tNovember 2016\nCourse:\t\tCOSC 320 - Analysis of Algorithms"); 
-		System.out.println("Authors:\t(Group 6)\n\t\t-Barrett\n\t\t-Brooke\n\t\t-Mark\n\t\t-Mitch\n\t\t-Zack\n");
+		System.out.println("Authors:\t(Group 6)\n\t\t-Barrett\n\t\t-Brooke\n\t\t-Mark\n\t\t-Mitch\n\t\t-Zach\n");
 		
-		//Create Instance
-		DayTraderStrength dts = new DayTraderStrength(10,5);
+		Random rdm = new Random();
 		
-		//Run Speed tests, based on criteria parameters
-		//!@# WARNING) ALGO A IS NOW IN EFFECT, AND LARGE NUMBER OF RECORDS WILL TAKE FOREVER...
-		System.out.println("Beginning Speed Tests:\n");
-		speedTest(dts, 8000, 365, true); //8,000 case
-		speedTest(dts, 500000, 365, false); //500,000 case
-		speedTest(dts, 5000000, 365, false);// 5,000,000 case ('multi-million');
+		DayTraderStrength dts = new DayTraderStrength();
 		
-		//End
-		System.out.println("\nEnd of Speed Tests.");
-	}//main
+//		int [] a = {-1, -3, -2, 7, -7, 54, -41, 8, -2, -4, -6, -30}; // 10
+//		
+//		int x = dts.algorithmAforStrength(a);
+//		int y = dts.algorithmBforStrength(a);
+//		
+//		System.out.println("\n\n\n***********\n\nActual Result: 18");
+//		
+//		System.out.println("Algorithm A: " + x);
+//		
+//		System.out.println("Algorithm B: " + y + "\n\n*************");
+		
+		
+		int [] profits = new int[30];
+		
+		for (int i = 0; i < 30; i++) {
+			profits[i] = rdm.nextInt(1000000) - 500000;
+		}
+		
+		System.out.println("Starting A");
+		long astart = System.currentTimeMillis();
+		int a = dts.algorithmAforStrength(profits);
+		long afinish = System.currentTimeMillis();
+		long atime = afinish-astart;
+		System.out.println("Algorithm A (10000) = " + a + " (calculated in " + atime + " milliseconds)");
+		
+		System.out.println("\nStarting B");
+		long bstart = System.currentTimeMillis();
+		int b = dts.algorithmBforStrength(profits);
+		long bfinish = System.currentTimeMillis();
+		long btime = bfinish-bstart;
+		System.out.println("Algorithm B (10000) = " + b + " (calculated in " + btime + " milliseconds)");
+	}
 
-}//class
+}
